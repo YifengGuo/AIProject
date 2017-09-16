20581a4d8f35f4c4912eeb499292eb99ae999eb4
YifengGuo <gyf940517@gmail.com>
2017-09-15 23:37:37 -0400

    find path by dfs

--- src/search_algorithm/SearchPathByDFS.java
+++ src/search_algorithm/SearchPathByDFS.java
@@ -38,19 +38,23 @@ public class SearchPathByDFS {
         Entry start = new Entry(0, 0, 0);
         Entry end = new Entry(maze.length - 1, maze.length - 1, 0);
         searchPath(stack, maze, visited, start, end);
+        // push end to stack if it finds a path
         if (!stack.isEmpty()) {
             stack.offerFirst(end);
         }
+        // reverse order to get path from start to end
         Deque<Entry> path = reverseStack(stack);
         while (!path.isEmpty()) {
             res.add(path.pollFirst());
         }
+        // track visited matrix
         for (int i = 0; i < visited.length; i++) {
             for (int j = 0; j < visited.length; j++) {
                 System.out.print(visited[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println();
         return res;
     }
 
@@ -62,7 +66,7 @@ public class SearchPathByDFS {
             if (next.x != -1) {
                 stack.offerFirst(next);
                 visited[next.x][next.y] = true;
-            } else {  // backtracking
+            } else {  // backtracking if current stack.peek() has no choice to go
                 stack.pollFirst();
                 continue;
             }
@@ -96,23 +100,20 @@ public class SearchPathByDFS {
 
     public static void main(String[] args) {
         int[][] maze = new GenerateRandomMaze().genereate();
-//        int[][] maze= new int[][]{
-//                {0,0,0,0,0},
-//                {0,1,0,1,0},
-//                {0,1,1,0,0},
-//                {0,1,1,0,1},
-//                {0,0,0,0,0}
-//        };
         List<Entry> res = new SearchPathByDFS().getPath(maze);
+        // print maze
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze.length; j++) {
                 System.out.print(maze[i][j] + " ");
             }
             System.out.println();
         }
+        // fail to find the path if stack is empty
         if (res.isEmpty()) {
-            System.out.print("Fail to find the path.");
+            System.out.println();
+            System.out.println("Fail to find the path.");
         }
+        System.out.println();
         for (Entry e : res) {
             System.out.print(e.toString() + " ");
         }
