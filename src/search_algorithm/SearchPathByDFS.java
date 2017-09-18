6c5e2b9e6643ebf81166df733f1dfa1daccabf4e
YifengGuo <gyf940517@gmail.com>
2017-09-17 23:49:55 -0400

    exchange wall and path

--- src/search_algorithm/SearchPathByDFS.java
+++ src/search_algorithm/SearchPathByDFS.java
@@ -2,10 +2,14 @@ package search_algorithm;
 
 import maze_generation.GenerateRandomMaze;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.io.File;
 
 /**
  * @author yifengguo
@@ -59,8 +63,8 @@ public class SearchPathByDFS {
         }
         Deque<Entry> stack = new LinkedList<>();
         boolean[][] visited = new boolean[maze.length][maze.length];
-        Entry start = new Entry(0, 0, 0);
-        Entry end = new Entry(maze.length - 1, maze.length - 1, 0);
+        Entry start = new Entry(0, 0, 1);
+        Entry end = new Entry(maze.length - 1, maze.length - 1, 1);
         searchPath(stack, maze, visited, start, end);
         // reverse order to get path from start to end
         Deque<Entry> path = reverseStack(stack);
@@ -94,17 +98,17 @@ public class SearchPathByDFS {
     }
 
     private Entry getAdjacentNotVisitedEntry(Entry e, boolean[][] visited, int[][] maze) {
-        if (e.x - 1 >= 0 && visited[e.x - 1][e.y] == false && maze[e.x - 1][e.y] == 0) {  // go up
-            return new Entry(e.x - 1, e.y, 0);
+        if (e.x - 1 >= 0 && visited[e.x - 1][e.y] == false && maze[e.x - 1][e.y] == 1) {  // go up
+            return new Entry(e.x - 1, e.y, 1);
         }
-        else if (e.x + 1 < maze.length && visited[e.x + 1][e.y] == false && maze[e.x + 1][e.y] == 0) {  // go down
-            return new Entry(e.x + 1, e.y, 0);
+        else if (e.x + 1 < maze.length && visited[e.x + 1][e.y] == false && maze[e.x + 1][e.y] == 1) {  // go down
+            return new Entry(e.x + 1, e.y, 1);
         }
-        else if (e.y - 1 >= 0 && visited[e.x][e.y - 1] == false && maze[e.x][e.y - 1] == 0) {  // go left
-            return new Entry(e.x, e.y - 1, 0);
+        else if (e.y - 1 >= 0 && visited[e.x][e.y - 1] == false && maze[e.x][e.y - 1] == 1) {  // go left
+            return new Entry(e.x, e.y - 1, 1);
         }
-        else if (e.y + 1 < maze[0].length && visited[e.x][e.y + 1] == false && maze[e.x][e.y + 1] == 0) {  // go right
-            return new Entry(e.x, e.y + 1, 0);
+        else if (e.y + 1 < maze[0].length && visited[e.x][e.y + 1] == false && maze[e.x][e.y + 1] == 1) {  // go right
+            return new Entry(e.x, e.y + 1, 1);
         }
         // no valid path to go
         return new Entry(-1, -1, -1);
@@ -116,6 +120,59 @@ public class SearchPathByDFS {
             path.offerFirst(stack.pollFirst());
         }
         return path;
+    }
+
+    private static void outputMaze(int[][] maze, List<Entry> list) {
+        File file = new File("src/data_visualization/DFS/dfs_maze.csv");
+        BufferedWriter bw = null;
+        // change path cell to 2
+        for (Entry e : list) {
+            maze[e.x][e.y] = 2;
+        }
+        try {
+            bw = new BufferedWriter(new FileWriter(file));
+            for (int i = 0; i < maze.length; i++) {
+                for (int j = 0; j < maze.length; j++) {
+                    if (j != maze.length - 1) {
+                        bw.write(maze[i][j] + ",");
+                    } else {
+                        bw.write(maze[i][j]+ "");
+                    }
+                }
+                bw.write("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void outputPath(List<Entry> list) {
+        File file = new File("src/data_visualization/DFS/dfs_path.csv");
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter(file));
+            for (Entry e : list) {
+                bw.write(e.x + "," + e.y + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -137,5 +194,8 @@ public class SearchPathByDFS {
         for (Entry e : res) {
             System.out.print(e.toString() + " ");
         }
+        // path visualization
+        outputMaze(maze, res);
+        outputPath(res);
     }
 }
