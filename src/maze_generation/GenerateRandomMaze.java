4ab969e6ca2160a99000cc07d8ecce863256ce85
YifengGuo <gyf940517@gmail.com>
2017-10-02 15:20:20 -0400

    update

--- src/maze_generation/GenerateRandomMaze.java
+++ src/maze_generation/GenerateRandomMaze.java
@@ -6,7 +6,7 @@ import java.util.Random;
  * @author yifengguo
  */
 public class GenerateRandomMaze {
-    private final static int dim = 15;
+    private final static int dim = 100;
     private final static double p0 = 0.0;
     private final static double p1 = 0.1;
     private final static double p2 = 0.3;
@@ -19,7 +19,7 @@ public class GenerateRandomMaze {
         for (int i = 0; i < dim; i++) {
             for (int j = 0; j < dim; j++) {
                 double p = rand.nextDouble(); // 0.0 - 1.0
-                if (p >= p2) {  // change wall occurrence probability
+                if (p >= p3) {  // change wall occurrence probability
                     maze[i][j] = 1; // path
                 } else {
                     maze[i][j] = 0; // wall
